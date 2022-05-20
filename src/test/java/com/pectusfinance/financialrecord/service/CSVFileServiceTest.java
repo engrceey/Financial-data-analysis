7132402891c5ba19309befefd3d70a6e7395f107
zurum <zurumogbonda@gmail.com>
2022-05-20 19:01:34 +0100

    bootstrap test

---
+++ src/test/java/com/pectusfinance/financialrecord/service/CSVFileServiceTest.java
@@ -0,0 +1,12 @@
+package com.pectusfinance.financialrecord.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CSVFileServiceTest {
+
+    @Test
+    void saveFileToDB() {
+    }
+}
---
+++ src/test/java/com/pectusfinance/financialrecord/service/ExpansesServiceTest.java
@@ -0,0 +1,30 @@
+package com.pectusfinance.financialrecord.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExpansesServiceTest {
+
+
+
+    @Test
+    void fetchExpanses() {
+    }
+
+    @Test
+    void fetchExpansesSorted() {
+    }
+
+    @Test
+    void fetchExpansesSortedByOneOrMoreFields() {
+    }
+
+    @Test
+    void filterByAmountOrMemberName() {
+    }
+
+    @Test
+    void fetchSumOfExpansesByDepartment() {
+    }
+}

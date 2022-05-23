bb0afe62b598b7a276dd230ff9163fd7cdfc558c
zurum <zurumogbonda@gmail.com>
2022-05-23 14:50:29 +0100

    update dataloarder

--- src/main/java/com/pectusfinance/financialrecord/bootstrap/DataLoader.java
+++ src/main/java/com/pectusfinance/financialrecord/bootstrap/DataLoader.java
@@ -3,32 +3,56 @@ package com.pectusfinance.financialrecord.bootstrap;
 
 import com.pectusfinance.financialrecord.entity.Expanse;
 import com.pectusfinance.financialrecord.repository.ExpansesRepository;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Component
-@RequiredArgsConstructor
 public class DataLoader {
 
-    private final ExpansesRepository expansesRepository;
-
-    public boolean loadData() {
-        Expanse expanseA = new Expanse( "pet", "ExtraWin", BigDecimal.valueOf(10000.90), LocalDate.parse("01/02/1199"),"Abidemi");
-        Expanse expanseB = new Expanse( "gas", "ExtraWin", BigDecimal.valueOf(10000.90), LocalDate.parse("01/02/1199"),"Abidemi");
-        Expanse expanseC = new Expanse( "mech", "ExtraWin", BigDecimal.valueOf(10000.90), LocalDate.parse("01/02/1199"),"Abidemi");
-        Expanse expanseD = new Expanse( "java", "ExtraWin", BigDecimal.valueOf(10000.90), LocalDate.parse("01/02/1199"),"Abidemi");
-        Expanse expanseE = new Expanse( "node", "ExtraWin", BigDecimal.valueOf(10000.90), LocalDate.parse("01/02/1199"),"Abidemi");
-        Expanse expanseF = new Expanse( "csharp", "ExtraWin", BigDecimal.valueOf(10000.90), LocalDate.parse("01/02/1199"),"Abidemi");
-        Expanse expanseG = new Expanse( "go", "ExtraWin", BigDecimal.valueOf(10000.90), LocalDate.parse("01/02/1199"),"Abidemi");
-        Expanse expanseH = new Expanse( "vue", "ExtraWin", BigDecimal.valueOf(10000.90), LocalDate.parse("01/02/1199"),"Abidemi");
-        Expanse expanseI = new Expanse( "ruby", "ExtraWin", BigDecimal.valueOf(10000.90), LocalDate.parse("01/02/1199"),"Abidemi");
-        Expanse expanseJ = new Expanse( "python", "ExtraWin", BigDecimal.valueOf(10000.90), LocalDate.parse("01/02/1199"),"Abidemi");
-
-        return true;
+    public DataLoader() {
     }
+
+    @Autowired
+    private ExpansesRepository expansesRepository;
+
+
+    public void loadData() {
+        log.info("Inside Data Loader");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(""
+                + "[M/dd/yyyy]"
+                + "[MM/dd/yyyy]"
+                + "[M/d/yyyy]"
+                + "[MM/d/yyyy]"
+        );
+
+        List<Expanse> expansesList = new ArrayList<>(List.of(new Expanse( "pet", "drilling", BigDecimal.valueOf(10000.90), LocalDate.parse("01/02/1991", formatter),"ceey"),
+            new Expanse( "gas", "gas lifting", BigDecimal.valueOf(10000.90), LocalDate.parse("01/02/1991", formatter),"bundu"),
+         new Expanse( "mech", "car", BigDecimal.valueOf(10000.90), LocalDate.parse("01/02/1999", formatter),"stephen"),
+         new Expanse( "java", "fintech app", BigDecimal.valueOf(10000.90), LocalDate.parse("01/02/1999", formatter),"zurum"),
+        new Expanse( "node", "blog", BigDecimal.valueOf(10000.90), LocalDate.parse("01/02/1998", formatter),"mark"),
+        new Expanse( "csharp", "Bank", BigDecimal.valueOf(10000.90), LocalDate.parse("01/02/1997", formatter),"louis"),
+         new Expanse( "go", "mail", BigDecimal.valueOf(10000.90), LocalDate.parse("01/02/1996", formatter),"victor"),
+         new Expanse( "vue", "instagram", BigDecimal.valueOf(10000.90), LocalDate.parse("01/02/1996", formatter),"kbiru"),
+         new Expanse( "ruby", "blog", BigDecimal.valueOf(10000.90), LocalDate.parse("01/02/1997", formatter),"negge"),
+         new Expanse( "python", "analysis", BigDecimal.valueOf(10000.90), LocalDate.parse("01/02/1990", formatter),"bella")
+
+        ));
+
+        log.info("List of Data");
+        if (expansesRepository.findById(1L).isEmpty())
+            {
+                log.info("Loading data to the DB");
+                expansesRepository.saveAll(expansesList);
+            }
+
+
+        }
 }
